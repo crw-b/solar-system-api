db32efa0cdec1b62dd1d2edb773d9abee52977d0
Christina Webber <chrisswebb@yahoo.com>
2022-04-25 14:04:42 -0500

    wave 2 completed

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    from .routes import bp
+    from .planet_routes import bp
     app.register_blueprint(bp)
 
 
