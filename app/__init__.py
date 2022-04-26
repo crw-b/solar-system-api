5baf234778d929edab5b89e5107b6d588512daa2
jae <j.kerr.clayton@gmail.com>
2022-04-26 17:05:02 -0500

    making a new branch for the love of halo

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 
 
+
+
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    from .planet_routes import bp
-    app.register_blueprint(bp)
+    from .routes import planet_routes , galaxies_routes 
+    
+    app.register_blueprint(planet_routes.bp)
+    app.register_blueprint(galaxies_routes.galaxy_bp)
 
 
     return app
