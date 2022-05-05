d5cfa9a9828192226fbf85e0ef8a91fe7c1d4ba3
Christina Webber <chrisswebb@yahoo.com>
2022-05-05 12:48:41 -0500

    tests

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -10,7 +12,11 @@ def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/planets_development'
+    if test_config is None:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_TEST_URI")
     
     db.init_app(app)
     migrate.init_app(app, db)
---
+++ app/tests/conftests.py
@@ -0,0 +1,33 @@
+import pytest
+from flask.signals import request_finished
+from app import db
+from app import create_app
+from app.models.planet import Planet
+
+@pytest.fixture
+def app():
+    app = create_app({"TESTING": True})
+
+    @request_finished.connect_via(app)
+    def expire_session(sender, response, **extra):
+        db.session.remove()
+    
+    with app.app_context():
+        db.create_all()
+        yield app
+    
+    with app.app_context():
+        db.drop_all()
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+@pytest.fixture
+def two_planets(app):
+    Earth = Planet(id=1, name="Earth", description="home", life=True, moons="luna")
+    Mars = Planet(id=2, name="Mars", description="1st Colony", life=True, moons=None)
+
+    db.session.add(Earth)
+    db.session.add(Mars)
+    db.session.commit()
