79815182bd32c8e338e91fd3caf2bc49dece0060
jae <j.kerr.clayton@gmail.com>
2022-05-06 12:11:36 -0500

    update tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,7 +7,7 @@ def test_get_all_planets_with_no_records(client):
     assert response.status_code == 200
     assert response_body == []
     
-def test_get_all_planeets(client, two_planets):
+def test_get_all_planets(client, two_planets):
     response = client.get('/planets')
 
     response_body = response.get_json()
